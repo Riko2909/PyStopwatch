0211dfa3d66b9a19bfaba6334c1e180d9ae8105e
rikorick <riko.gerdes@gmail.com>
2023-03-27 15:53:00 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,61 @@
 import time
-import keyboard
+import os
 from tkinter import *
 from tkinter.ttk import *
+import RPi.GPIO as GPIO
+
+# used for dev
+if os.environ.get('DISPLAY', '') == '':
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
+
 
 class LabelInit:
-    def __init__(self, x, y):
+    def __init__(self, x, y, pin):
         self.x = x
         self.y = y
+        self.pin = pin
         self.time = 0
         self.stopped = False
         self.label = None
+
+    def initPin(self):
+        GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     def initLabel(self, label):
         self.label = label
 
     def stopTime(self):
+        print(f"Button {self.pin} wurde gestoppt!")
         self.time = time.time() - start_zeit
         self.stopped = True
+
 
 start_zeit = 0
 labelcontainer: dict[str, LabelInit] = {}
 started = False
 
 labelInit = {
-    "1" : LabelInit(200, 12),
-    "2" : LabelInit(200, 42),
-    "3" : LabelInit(200, 72),
-    "4" : LabelInit(200, 102),
-    "5" : LabelInit(200, 132),
-    "6" : LabelInit(200, 162),
+    "1": LabelInit(285, 80, 17),
+    "2": LabelInit(285, 260, 27),
+    "3": LabelInit(285, 440, 22),
+    "4": LabelInit(720, 80, 5),
+    "5": LabelInit(720, 260, 6),
+    "6": LabelInit(720, 440, 26)
 }
+
+gpiolist = [23, 24]
+
+
+def initGPIO():
+    GPIO.setmode(GPIO.BCM)
+
+    for gpio in gpiolist:
+        GPIO.setup(gpio, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+initGPIO()
+
 
 def start():
     global start_zeit, started
@@ -37,10 +63,13 @@ def start():
 
     if len(labelcontainer) == 0:
         for i in labelInit:
+            curlabel = labelInit.get(i)
+
             label = Label(root, text="0:00")
-            labelcontainer[i] = labelInit.get(i)
+            labelcontainer[i] = curlabel
             labelcontainer[i].initLabel(label)
-            label.place(x=labelInit.get(i).x, y=labelInit.get(i).y)
+            label.place(x=curlabel.x, y=curlabel.y)
+            curlabel.initPin()
     else:
         for counter in labelcontainer:
             labelcontainer.get(counter).stopped = False
@@ -49,18 +78,27 @@ def start():
 
     updateLoop()
 
+
 def updateLoop():
     while started:
         for counter in labelcontainer:
-            # stops program completely
-            time.sleep(0.01)
-            if not labelcontainer.get(counter).stopped:
-                labelcontainer.get(counter).label.config(text="{0:.3f}".format(time.time() - start_zeit))
+            curlable: LabelInit = labelcontainer.get(counter)
+
+            if GPIO.input(curlable.pin) and not curlable.stopped:
+                stop(counter)
+
+            if not curlable.stopped:
+                curlable.label.config(
+                    text="{0:.3f}".format(time.time() - start_zeit))
+
+        if GPIO.input(gpiolist[1]):
+            stopAll()
 
         root.update()
 
+
 def stopAll():
-    global labelcontainer
+    global labelcontainer, started
 
     for counter in labelcontainer:
         curlabel = labelcontainer.get(counter)
@@ -69,17 +107,8 @@ def stopAll():
 
     started = False
 
-def startbutton():
-    start()
-    label = Label(root, text=("Die Stopuhr wurde Gestartet"))
-    label.place(x=80, y=190)
 
-def resetbutton():
-    stopAll()
-    label = Label(root, text=("Die Stopuhr wurde Gestoppt"))
-    label.place(x=80, y=190)
-
-def stop (h, v, buttonID):
+def stop(buttonID: str):
     global start_zeit
     global labelcontainer
 
@@ -92,20 +121,38 @@ def stop (h, v, buttonID):
     else:
         labelcontainer.get(f"{buttonID}").stopTime()
 
+
 root = Tk()
 root.title("stopuhr")
-root.geometry("300x220")
+root.geometry("1000x1000")
 root.configure(bg="#86354A")
 
-start_button = Button(root, text="Start", command=startbutton).place(x=15, y=10)
 
-stop_button1 = Button(root, text="Stop", command=lambda: stop(200, 12, 1)).place(x=115, y=10)
-stop_button2 = Button(root, text="Stop", command=lambda: stop(200, 42, 2)).place(x=115, y=40)
-stop_button3 = Button(root, text="Stop", command=lambda: stop(200, 72, 3)).place(x=115, y=70)
-stop_button4 = Button(root, text="Stop", command=lambda: stop(200, 102, 4)).place(x=115, y=100)
-stop_button5 = Button(root, text="Stop", command=lambda: stop(200, 132, 5)).place(x=115, y=130)
-stop_button6 = Button(root, text="Stop", command=lambda: stop(200, 162, 6)).place(x=115, y=160)
+def initStopButton(y_start):
+    Button(root, text="Bahn 1", command=lambda: stop(1)).place(x=35, y=y_start+30, height=100, width=200)
+    Button(root, text="Bahn 2", command=lambda: stop(2)).place(x=35, y=y_start+30*7, height=100, width=200)
+    Button(root, text="Bahn 3", command=lambda: stop(3)).place(x=35, y=y_start+30*13, height=100, width=200)
+    Button(root, text="Bahn 4", command=lambda: stop(4)).place(x=470, y=y_start+30, height=100, width=200)
+    Button(root, text="Bahn 5", command=lambda: stop(5)).place(x=470, y=y_start+30*7, height=100, width=200)
+    Button(root, text="Bahn 6", command=lambda: stop(6)).place(x=470, y=y_start+30*13, height=100, width=200)
+    
 
-reset_button = Button(root, text="Reset", command=resetbutton).place(x=15, y=40)
 
+start_button = Button(root, text="Start",
+                      command=start).place(x=850, y=50, height=75, width=125)
+
+initStopButton(10)
+
+def isStartButtonPressed():
+    if GPIO.input(gpiolist[0]) and not started:
+        start()
+
+    root.after(10, isStartButtonPressed)
+        
+
+
+reset_button = Button(root, text="Reset",
+                      command=stopAll).place(x=850, y=410, height=75, width=125)
+root.after(10, isStartButtonPressed)
 root.mainloop()
+GPIO.cleanup()
